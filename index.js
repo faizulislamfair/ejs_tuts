64c609f09c14a12f8b0c21fbb2c843b5548ecec0
faizulislamfair <faizulislamfair@gmail.com>
2022-05-22 21:59:21 +0600

    passing data to server and receiving at ejs file

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+app.use(express.urlencoded({extended: true}))
+
 app.set("view engine", "ejs")
 
+
+let pLanguages = []
+
+
 app.get('/', (req, res)=>{
-    res.render("index", {});
+    res.render("index", {plNames: pLanguages});
 })
+
+app.post('/', (req, res)=>{
+     const pLanguage = req.body.pLanguage;
+     pLanguages.push(pLanguage);
+     res.redirect('/');
+})
+
 
 app.listen(PORT, ()=>{
     console.log(`App is listening at http://localhost:${PORT}`);
